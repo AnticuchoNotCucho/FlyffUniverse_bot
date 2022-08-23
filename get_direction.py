d0d7d1cfff59a7f9e8e44830875abdd5a7379afe
anticuchonotcucho <jorge_2jul@hotmail.com>
2022-08-23 04:18:58 -0400

    Creado archivo testing para probar codigo, get_direction detectando exitosamente flecha en el mapa.

--- get_direction.py
+++ get_direction.py
@@ -1,5 +1,8 @@
 import cv2
+import numpy as np
+import sys
 
+np.set_printoptions(threshold=sys.maxsize)
 method = cv2.TM_SQDIFF_NORMED
 
 map = cv2.imread('laberintiniti.png', 0)
@@ -9,9 +12,13 @@ result = cv2.matchTemplate(arrow, map, method)
 print(result)
 mn, _, mnLoc, _ = cv2.minMaxLoc(result)
 MPx, MPy = mnLoc
+print(MPx, MPy)
 trows, tcols = arrow.shape[:2]
-cv2.rectangle(map, (MPx, MPy), (MPx + tcols, MPy + trows), (0, 0, 255), 2)
-
+print(MPx + tcols, MPy + trows)
+MPx = MPx + 20
+MPy = MPy + 20
+rectangle = cv2.rectangle(map, (MPx, MPy), (MPx + (tcols - 40), MPy + (trows - 20)), (0, 0, 255), 2)
+print(len(rectangle))
 # Display the original image with the rectangle around the match.
 cv2.imshow('output', map)
 
